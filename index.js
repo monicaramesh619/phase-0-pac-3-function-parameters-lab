61190a2ea8d9158d630290b825ae25433d40c4a7
Monica Ramesh <monica98r@gmail.com>
2023-01-25 13:07:47 -0500

    commit tree 563e74125052cc99532aa04c17f44ecc84153784

---
+++ index.js
@@ -0,0 +1,6 @@
+function introduction(firstName) {
+    console.log(`Hello, ${Aki}!`);
+  }
+  function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
